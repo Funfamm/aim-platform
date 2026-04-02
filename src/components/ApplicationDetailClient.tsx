2ca48ae6d6fc313d6eb7d461c37f5a0892e8f61f
aiimpactmediastudio-gif <ai.impactmediastudio@gmail.com>
2026-04-02 08:02:07 -0400

    fix: ApplicationDetailClient crashes in admin - useRouter must be from next/navigation not i18n

--- src/components/ApplicationDetailClient.tsx
+++ src/components/ApplicationDetailClient.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { useRouter } from '@/i18n/navigation'
+import { useRouter } from 'next/navigation'
 import AICastingReport from '@/components/application/AICastingReport'
 
 interface Props {
